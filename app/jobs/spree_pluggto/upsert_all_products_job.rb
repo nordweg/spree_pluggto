041a2000f57653aa71b22e1e7d08a22d9b35587f
Lucas Kuhn <lucasalexkuhn@gmail.com>
2021-07-26 14:48:18 -0300

    Adds namespace

--- app/jobs/spree_pluggto/upsert_all_products_job.rb
+++ app/jobs/spree_pluggto/upsert_all_products_job.rb
@@ -2,7 +2,7 @@ module SpreePluggto
   class UpsertAllProductsJob < ActiveJob::Base
     def perform
      ::Spree::Product.active.each do |product|
-       UpsertProductJob.perform_now(product.id)
+       SpreePluggto::UpsertProductJob.perform_now(product.id)
       end
     end
   end
