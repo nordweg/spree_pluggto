24db4648aeac24bf815654b0ecfbc3d67e18362f
Lucas Kuhn <lucasalexkuhn@gmail.com>
2021-08-17 17:13:56 -0300

    comment out sidekiq_retries_exhausted

--- app/jobs/spree_pluggto/order_created_job.rb
+++ app/jobs/spree_pluggto/order_created_job.rb
@@ -1,11 +1,12 @@
 module SpreePluggto
   class OrderCreatedJob < ActiveJob::Base
 
-    sidekiq_retries_exhausted do |msg, ex|
-      message = "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
-      ::SpreePluggto::SendIntegrationErrors.new(msg['args']).call(message)
-    end
     
+    # sidekiq_retries_exhausted do |msg, ex|
+    #   message = "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
+    #   ::SpreePluggto::SendIntegrationErrors.new(msg['args']).call(message)
+    # end
+
     def perform(pluggto_id)
       ::SpreePluggto::CreateSpreeOrder.new(pluggto_id).call
     end
