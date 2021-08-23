96ac17050a065767121a216ed53a4b0f258c1f0d
Lucas Kuhn <lucasalexkuhn@gmail.com>
2021-08-23 13:22:43 -0300

    Release version 1 to ruby gems

--- lib/spree_pluggto/version.rb
+++ lib/spree_pluggto/version.rb
@@ -8,9 +8,9 @@ module SpreePluggto
   end
 
   module VERSION
-    MAJOR = 0
+    MAJOR = 1
     MINOR = 0
-    TINY  = 2
+    TINY  = 0
     # PRE   = 'alpha'.freeze
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
